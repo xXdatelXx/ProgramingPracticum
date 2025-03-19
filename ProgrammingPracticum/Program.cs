828b22db197abc7bb23901c7a1b1edc65b0c6d12
xXdatelXx <artemgurv@gmail.com>
2025-03-19 12:40:45 +0200

    Second practicum first exercise

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -1,58 +1,57 @@
-﻿using Newtonsoft.Json;
+﻿Time time = new Time(23, 59, 50);
+Console.WriteLine(time);  // 23:59:50
+time.AddTime(0, 0, 15);
+Console.WriteLine(time);  // 00:00:05
 
-// Exercise 1
-Console.WriteLine("Exercise 1");
-Console.WriteLine("Input list: ");
+class Time(int Hours, int Minutes, int Seconds)
+{
+   public void SetTime(int hours, int minutes, int seconds)
+   {
+      if (hours < 0 || hours >= 24 || minutes < 0 || minutes >= 60 || seconds < 0 || seconds >= 60)
+         throw new ArgumentException("Недопустиме значення часу");
+      
+      Hours = hours;
+      Minutes = minutes;
+      Seconds = seconds;
+   }
 
-List<int> list = [1, 2, 3, 4, 5];
-list.ForEach(Console.Write);
-for (int i = 0; i < list.Count - 1; i += 2) 
-   (list[i], list[i + 1]) = (list[i + 1], list[i]);
+   public void SetHours(int hours)
+   {
+      if (hours is < 0 or >= 24)
+         throw new ArgumentException("Недопустиме значення годин");
+      
+      Hours = hours;
+   }
 
-Console.WriteLine("\nOutput list: ");
-list.ForEach(Console.Write);
-Console.WriteLine();
+   public void SetMinutes(int minutes)
+   {
+      if (minutes is < 0 or >= 60)
+         throw new ArgumentException("Недопустиме значення хвилин");
+      
+      Minutes = minutes;
+   }
 
+   public void SetSeconds(int seconds)
+   {
+      if (seconds < 0 || seconds >= 60)
+         throw new ArgumentException("Недопустиме значення секунд");
+      
+      Seconds = seconds;
+   }
 
+   public void AddTime(int hours = 0, int minutes = 0, int seconds = 0)
+   {
+      int totalSeconds = Hours * 3600 + Minutes * 60 + Seconds;
+      totalSeconds += hours * 3600 + minutes * 60 + seconds;
+      totalSeconds %= 86400; // 24 години в секундах
 
-// Exercise 2
-Console.WriteLine("Exercise 2");
-Console.WriteLine("Input dictionary: ");
+      Hours = totalSeconds / 3600;
+      Minutes = (totalSeconds % 3600) / 60;
+      Seconds = totalSeconds % 60;
+   }
 
-Dictionary<int, int> dict = new() {
-   {1, 50}, {2, 10}, {3, 90}, {4, 30}, {5, 70}
-};
-
-dict.ToList().ForEach(kv => Console.WriteLine($"Key: {kv.Key}, Value: {kv.Value}"));
-
-int min = dict.Values.Min();
-int max = dict.Values.Max();
-
-Console.WriteLine("Min value: " + min + ", Max value: " + max);
-
-foreach (var key in dict.Where(kv => kv.Value == min || kv.Value == max).Select(kv => kv.Key)) 
-   dict.Remove(key);
-
-dict = dict.OrderBy(kv => kv.Key).ToDictionary(k => k.Key, v => v.Value);
-
-Console.WriteLine("Output dictionary: ");
-dict.ToList().ForEach(kv => Console.WriteLine($"Key: {kv.Key}, Value: {kv.Value}"));
-
-string json = JsonConvert.SerializeObject(dict, Formatting.Indented);
-
-// ProgrammingPracticum -> ProgrammingPracticum -> bin -> Release -> .exe
-string path = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
-File.WriteAllText(Path.Combine(path, "dictionary.json"), json);
-
-
-
-// Exercise 3
-Console.WriteLine("Exercise 3");
-Console.WriteLine("Input list: ");
-
-List<int> list2 = [-5, 3, 8, -2, 7, 0, 6, -1, 9];
-list2.ForEach(i => Console.Write(i + " "));
-
-Console.WriteLine();
-Console.WriteLine($"List2 where i > 0: {list2.Count(i => i > 0)}");
-Console.WriteLine($"List2 sum where i % 2 != 0: {list2.Where(i => i % 2 != 0).Sum()}");
+   public override string ToString()
+   {
+      return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+   }
+}
