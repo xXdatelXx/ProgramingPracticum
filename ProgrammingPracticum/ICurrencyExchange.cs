119103e62e7ad2e6de7ad6295f0d064a8731e782
xXdatelXx <artemgurv@gmail.com>
2025-04-02 13:14:55 +0300

    Cleanup

--- ProgrammingPracticum/ICurrencyExchange.cs
+++ ProgrammingPracticum/ICurrencyExchange.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProgrammingPracticum;
 
 public interface ICurrencyExchange {
-   decimal Convert(Currency from, Currency to, decimal amount);
    decimal Balance(Currency currency);
+   decimal Convert(Currency from, Currency to, decimal amount);
 }
